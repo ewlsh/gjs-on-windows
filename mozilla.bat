06658375099da188e95dfa79cb0996f65f60ed06
Evan Welsh <contact@evanwelsh.com>
2021-08-07 03:12:57 -0700

    Try copying the file...

--- mozilla.bat
+++ mozilla.bat
@@ -2,6 +2,8 @@ curl -s https://ftp.mozilla.org/pub/mozilla.org/mozilla/libraries/win32/MozillaB
 
 MozillaBuildSetup-Latest.exe /S
 
+copy D:\a\gjs-on-windows\gjs-on-windows\mozjs.sh C:\mozilla-build\mozjs.sh
+
 echo "ac_add_options --enable-nspr-build">D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
 echo.>>D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
 echo.>>D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
