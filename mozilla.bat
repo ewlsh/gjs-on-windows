121066dcadc8ae9bd12e0b4d0c24136a9a3415d1
Evan Welsh <contact@evanwelsh.com>
2021-08-07 02:35:08 -0700

    Use custom shell

---
+++ mozilla.bat
@@ -0,0 +1,12 @@
+cd C:\
+
+git clone https://github.com/ptomato/mozjs
+
+curl -s https://ftp.mozilla.org/pub/mozilla.org/mozilla/libraries/win32/MozillaBuildSetup-Latest.exe -o MozillaBuildSetup-Latest.exe
+
+MozillaBuildSetup-Latest.exe /S
+
+echo "ac_add_options --enable-nspr-build">D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
+echo.>>D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
+echo.>>D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
+echo "ac_add_options --disable-jemalloc">>D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
--- mozjs.bat
+++ mozjs.bat
@@ -1,18 +1,3 @@
-cd C:\
-
-git clone https://github.com/ptomato/mozjs
-
-curl -s https://ftp.mozilla.org/pub/mozilla.org/mozilla/libraries/win32/MozillaBuildSetup-Latest.exe -o MozillaBuildSetup-Latest.exe
-
-MozillaBuildSetup-Latest.exe /S
-
-echo "ac_add_options --enable-nspr-build">D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
-echo.>>D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
-echo.>>D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
-echo "ac_add_options --disable-jemalloc">>D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
-
-C:\mozilla-build\start-shell.bat
-
 cd mozjs
 
 SET MOZCONFIG=D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
