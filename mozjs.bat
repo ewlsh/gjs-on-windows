0b942273ce0ae078a414e499f8c5d1b1869cb4a6
Evan Welsh <contact@evanwelsh.com>
2021-08-07 02:19:44 -0700

    Temp

--- mozjs.bat
+++ mozjs.bat
@@ -2,20 +2,20 @@ cd C:\
 
 git clone https://github.com/ptomato/mozjs
 
-curl -s https://ftp.mozilla.org/pub/mozilla.org/mozilla/libraries/win32/MozillaBuildSetup-Latest.exe -o C:\MozillaBuildSetup-Latest.exe
+curl -s https://ftp.mozilla.org/pub/mozilla.org/mozilla/libraries/win32/MozillaBuildSetup-Latest.exe -o MozillaBuildSetup-Latest.exe
 
-MozillaBuildSetup-Latest.exe /S /D=C:\\mozilla-build
+MozillaBuildSetup-Latest.exe /S /D=D:\a\gjs-on-windows\gjs-on-windows\mozilla-build
 
-echo "ac_add_options --enable-nspr-build\r\n">C:\mozconfigs\win32
-echo.>>C:\mozconfigs\win32
-echo.>>C:\mozconfigs\win32
-echo "ac_add_options --disable-jemalloc">>C:\mozconfigs\win32
+echo "ac_add_options --enable-nspr-build">D:\a\gjs-on-windows\gjs-on-windows\mozconfigs\win32
+echo.>>D:\a\gjs-on-windows\gjs-on-windows\mozconfigs\win32
+echo.>>D:\a\gjs-on-windows\gjs-on-windows\mozconfigs\win32
+echo "ac_add_options --disable-jemalloc">>D:\a\gjs-on-windows\gjs-on-windows\mozconfigs\win32
 
-C:\mozilla-build\start-shell.bat
+D:\a\gjs-on-windows\gjs-on-windows\mozilla-build\start-shell.bat
 
 cd mozjs
 
-export MOZCONFIG=C:\mozconfigs\win32
+export MOZCONFIG=D:\a\gjs-on-windows\gjs-on-windows\mozconfigs\win32
 export JS_STANDALONE=1
 
 ./mach build
