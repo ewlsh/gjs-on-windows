0d4ce7011461ae72d912ac0c90d938f3800bd5af
Evan Welsh <contact@evanwelsh.com>
2021-08-07 02:40:36 -0700

    Setup bash correctly

--- mozjs.bat
+++
@@ -1,7 +0,0 @@
-cd mozjs
-
-SET MOZCONFIG=D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
-SET JS_STANDALONE=1
-
-./mach build
-
---
+++ mozjs.sh
@@ -0,0 +1,7 @@
+cd mozjs
+
+export MOZCONFIG="D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32"
+export JS_STANDALONE=1
+
+./mach build
+
