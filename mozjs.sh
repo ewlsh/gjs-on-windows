cb2a6a44db95349343281e0b80e6f7e905e0e6f5
Evan Welsh <contact@evanwelsh.com>
2021-08-07 03:52:08 -0700

    Update clone source...

--- mozjs.sh
+++ mozjs.sh
@@ -1,6 +1,6 @@
-git clone https://github.com/ptomato/mozjs
+git clone https://github.com/mozilla/gecko-dev/ --depth 1 -b esr78 --single-branch mozilla-esr78
 
-cd mozjs
+cd mozilla-esr78
 
 export MOZCONFIG="D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32"
 export JS_STANDALONE=1
