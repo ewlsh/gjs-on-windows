55d82288ad7f65e59aa7393cbe79e6805d9ee1ac
Evan Welsh <contact@evanwelsh.com>
2021-08-07 03:58:10 -0700

    Only add one option for now...

--- mozilla.bat
+++ mozilla.bat
@@ -2,7 +2,5 @@ curl -s https://ftp.mozilla.org/pub/mozilla.org/mozilla/libraries/win32/MozillaB
 
 MozillaBuildSetup-Latest.exe /S
 
-echo "ac_add_options --enable-nspr-build">D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
-echo.>>D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
-echo.>>D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
-echo "ac_add_options --disable-jemalloc">>D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
+echo "ac_add_options --disable-jemalloc">D:\a\gjs-on-windows\gjs-on-windows\mozconfig-win32
+
